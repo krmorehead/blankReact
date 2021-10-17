d1812b5f9c30989dceeb594deb4e95f28eb6d2b2
Kyle Morehead <kyle@limelighthealth.com>
2021-10-17 11:52:06 -0700

    feat: attempt 1 simple but limited

---
+++ web/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Sandbox} from "./sandbox/Sandbox";
+
+ReactDOM.render(
+  <Sandbox/>,
+  document.getElementById('root')
+);
---
+++ web/sandbox/Sandbox.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const Sandbox = () => {
+  return <div>
+    <h2>Welcome to The Sandbox !</h2>
+  </div>;
+};
