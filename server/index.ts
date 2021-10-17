a014d6792f43f8ed31c304731931a0b82bac5830
Kyle Morehead <kyle@limelighthealth.com>
2021-10-17 13:21:35 -0700

    feat: running with a FE in development

--- server/index.ts
+++ server/index.ts
@@ -7,9 +7,13 @@ const port = process.env.port || 8080;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
+
+app.get('/healthcheck', (req, res) => {
+  res.send('all is well with the world');
+})
 
 app.listen(port, () => {
   console.log( `server started at http://localhost:${ port }` );
